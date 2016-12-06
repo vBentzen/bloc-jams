c5fbfa0d7933238808fa208fd8a818983d626bcb
Aske Bentzen <askebentzen@gmail.com>
2016-12-06 15:40:08 +0100

    cp 9 complete, add scroll animation trigger

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,5 +1,7 @@
-var animatePoints = function () {
-	var points = document.getElementsByClassName('point');
+var pointsArray = document.getElementsByClassName('point');
+
+var animatePoints = function(points) {
+	
 	
 	var revealPoint = function (index) {
 		points[index].style.opacity = 1;
@@ -7,9 +9,23 @@ var animatePoints = function () {
 		points[index].style.msTransform = "scaleX(1)";
 		points[index].style.WebkitTransform = "scaleX(1)";
 	}
-	/* Hvordan fanger vi flere?, vil gerne kunne køre de 2 første fra højre, og så sidste [i] ind fra venstre, altså skal vi have noget i stil med en if index [0,1], do revealpoint, else do =? */
+	
 	for (var i = 0; i < points.length; i++) {
   revealPoint(i);
 	}
-
 };
+
+	window.onload = function() {
+		// Automatically animates the points on a bigger screen, that wont trigger scroll animation
+		if (window.innerHeight > 950) {
+			animatePoints(pointsArray);
+		}
+ 		var sellingPoints = document.getElementsByClassName('selling-points')[0];
+		var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
+
+	window.addEventListener("scroll", function(event) {
+  	if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
+			animatePoints(pointsArray);
+		}
+  });
+}
