f4c93044fc0280b362709f4ce670a25b61eace76
Aske Bentzen <askebentzen@gmail.com>
2016-12-06 14:43:04 +0100

    add fadeIn effect on selling points - cleaning up landing.js

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,29 +1,15 @@
 var animatePoints = function () {
 	var points = document.getElementsByClassName('point');
-
-	var revealFirstPoint = function () {
-		points[0].style.opacity = 1;
-		points[0].style.transform = "scaleX(1) translateY(0)";
-		points[0].style.msTransform = "scaleX(1) translateY(0)";
-		points[0].style.WebkitTransform = "scaleX(1) translateY(0)";
+	
+	var revealPoint = function (index) {
+		points[index].style.opacity = 1;
+		points[index].style.transform = "scaleX(1)";
+		points[index].style.msTransform = "scaleX(1)";
+		points[index].style.WebkitTransform = "scaleX(1)";
+	}
+	/* Hvordan fanger vi flere?, vil gerne kunne køre de 2 første fra højre, og så sidste [i] ind fra venstre, altså skal vi have noget i stil med en if index [0,1], do revealpoint, else do =? */
+	for (var i = 0; i < points.length; i++) {
+  revealPoint(i);
 	}
 
-	var revealSecondPoint = function() {
-		points[1].style.opacity = 1;
-		points[1].style.transform = "scaleX(1) translateY(0)";
-		points[1].style.msTransform = "scaleX(1) translateY(0)";
-		points[1].style.WebkitTransform = "scaleX(1) translateY(0)";
-	};
-
-	var revealThirdPoint = function() {
-		points[2].style.opacity = 1;
-		points[2].style.transform = "scaleX(1) translateY(0)";
-		points[2].style.msTransform = "scaleX(1) translateY(0)";
-		points[2].style.WebkitTransform = "scaleX(1) translateY(0)";
-	};
-
-	revealFirstPoint();
-	revealSecondPoint();
-	revealThirdPoint();
-
-}
+};
