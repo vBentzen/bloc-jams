3a84c0dce54e49aec72426b4cf5d8271ffdc0245
Aske Bentzen <askebentzen@gmail.com>
2016-12-12 12:07:10 +0100

    add getSongItem() method

--- scripts/album.js
+++ scripts/album.js
@@ -78,6 +78,27 @@ var findParentByClassName = function(element, targetClass) {
         return currentParent;
     }
 };
+
+
+var getSongItem = function(element) {
+    switch (element.className) {
+        case 'album-song-button':
+        case 'ion-play':
+        case 'ion-pause':
+            return findParentByClassName(element, 'song-item-number');
+        case 'album-view-song-item':
+            return element.querySelector('.song-item-number');
+        case 'song-item-title':
+        case 'song-item-duration':
+            return findParentByClassName(element, 'album-view-song-item').querySelector('.song-item-number');
+        case 'song-item-number':
+            return element;
+        default:
+            return console.log("not Found");
+    }
+};
+
+
 //create a var using a class(starting from index 0)
 var songListContainer = document.getElementsByClassName('album-view-song-list')[0];
 var songRows = document.getElementsByClassName('album-view-song-item');
